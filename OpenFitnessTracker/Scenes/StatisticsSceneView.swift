649727420182531b1641dd5fc91011bb42390273
Kantagonist <afk24rpct@protonmail.com>
2023-08-04 15:53:57 +0200

    cleanup statistics view

--- OpenFitnessTracker/Scenes/StatisticsSceneView.swift
+++ OpenFitnessTracker/Scenes/StatisticsSceneView.swift
@@ -1,5 +1,5 @@
 //
-//  PersonalInformationSceneView.swift
+//  StatisticsSceneView.swift
 //  OpenFitnessTracker
 //
 //  Created by Kantagonist on 23.05.23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-/// Small overview on user
+/// Page which displays certain user statistics pulled form existing workouts
 struct StatisticsSceneView: View {
     var body: some View {
         Text("Under construction")
@@ -16,16 +16,8 @@ struct StatisticsSceneView: View {
 
 // MARK: Preview
 
-/// Example user
-let previewExamplePerson = Person(
-    foto: UIImage(imageLiteralResourceName: "person_icon"),
-    name: "Max Mustermann",
-    birthdate: Date.now
-)
-
 struct PersonalInformationSceneView_Previews: PreviewProvider {
     static var previews: some View {
         StatisticsSceneView()
     }
 }
-
