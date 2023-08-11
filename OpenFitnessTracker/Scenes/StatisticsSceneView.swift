1a9cd9ef7672ef1742538bc2070912214a1f04e1
Kantagonist <afk24rpct@protonmail.com>
2023-08-11 15:03:13 +0200

    max values in statistics screen

--- OpenFitnessTracker/Scenes/StatisticsSceneView.swift
+++ OpenFitnessTracker/Scenes/StatisticsSceneView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 /// Page which displays certain user statistics pulled form existing workouts
 struct StatisticsSceneView: View {
+
+    @StateObject private var viewModel = ViewModel.getInstance()
+    
+    @State private var maxStrengthWorkout = ""
+    @State private var maxEnduranceWorkout = ""
+
+    // MARK: Main View
+
     var body: some View {
-        Text("Under construction")
+        
+        NavigationView {
+            List {
+                Section(header: Text("Max Values")) {
+                    VStack(spacing: 5) {
+                        ForEach(viewModel.settings.endWorkouts, id: \.self) { workout in
+                            HStack {
+                                Text(workout)
+                                Spacer()
+                                Text("\(String(format: "%.4f", viewModel.getHighestEnduranceDistance(for: workout))) \(viewModel.settings.distanceUnit.rawValue)")
+                            }
+                        }
+                    }
+                    VStack(spacing: 5) {
+                        ForEach(viewModel.settings.strWorkouts, id: \.self) { workout in
+                            HStack {
+                                Text(workout)
+                                Spacer()
+                                Text("\(String(format: "%.2f", viewModel.getHighestStrengthWeight(for: workout))) \(viewModel.settings.weightUnit.rawValue)")
+                            }
+                        }
+                    }
+                }
+
+                
+                
+            }
+            .foregroundColor(viewModel.settings.textColor)
+            .navigationBarTitle("Statistics")
+        }
     }
 }
 
